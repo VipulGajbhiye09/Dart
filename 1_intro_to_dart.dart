849245bfe20b4e72cae9c6e05249bd1be3d49eb8
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-26 23:33:31 +0530

    Create 1_intro_to_dart.dart

---
+++ 1_intro_to_dart.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  //print statements
+	print("Hello World"); 			
+	print("This is my first dart code");
+  
+  //data types
+  int num1 = 15;
+  double num2 = 93.4;
+	var count = 5;
+  const pi =3.14;
+  String name = "Joseph";
+  //boolean
+	bool isValid = true;
+	var isPresent = false;
+
+}
