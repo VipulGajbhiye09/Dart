4a0998a23b23214468c9bde2ccebfccf1806ad6c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-29 23:36:12 +0530

    Create 4_collections.dart

---
+++ 4_collections.dart
@@ -0,0 +1,14 @@
+//List : contains homogeneous entities
+var names1 = ['foo','bar''abc']  
+  
+// Sets : contains unique entities
+var names2 = {'foo','bar''abc'}  
+names2.add('foo');
+print(names);      //code executes but 'foo' wont be added to the set
+
+//Maps : It's a data structure which holds key-value pair of information
+
+const person = {      // keys are unique
+  'age':20,
+  'name':'Vipul'
+}
