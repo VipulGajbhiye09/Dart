a52c45d0c9ef386e3c49d434a8d5e70225e94683
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-29 23:32:51 +0530

    Create collections.dart

---
+++ collections.dart
@@ -0,0 +1,7 @@
+//List : contains homogeneous entities
+var names1 = ['foo','bar''abc']  
+  
+// Sets : contains unique entities
+var names2 = {'foo','bar''abc'}  
+names2.add('foo');
+print(names);      //code executes but 'foo' wont be added to the set
