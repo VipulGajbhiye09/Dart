b66323c68228433c7967fdb6cd13979c23ad8100
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-01 22:54:33 +0530

    Update 5_null_safety.dart

--- 5_null_safety.dart
+++ 5_null_safety.dart
@@ -5,3 +5,11 @@ const String? name = null;  //tells dart that string may be null sometimes
 
 int? age =20;
 age =null;
+
+//can be used in decision statements to check null 
+
+List <String>? names = ["foo","bar"];  //this means list can be null but not the name values in it
+
+List <String>? names = ["foo", null];  //gives error
+
+List <String>? names? = ["foo", null];  //no error
