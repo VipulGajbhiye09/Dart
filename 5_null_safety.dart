ecb1cccaa5ab182b5cbbdc0c766387ccd52645c2
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-01 22:58:52 +0530

    Update 5_null_safety.dart

--- 5_null_safety.dart
+++ 5_null_safety.dart
@@ -13,3 +13,12 @@ List <String>? names = ["foo","bar"];  //this means list can be null but not the
 List <String>? names = ["foo", null];  //gives error
 
 List <String>? names? = ["foo", null];  //no error
+
+//?? is null aware operator which picks not null operands from left to right
+
+//( xNotNull = a ?? b ?? c ?? ......) checks for not null and takes in first not nnull from left to right
+
+const String? fName = null;
+const String? lName = 'bar';
+
+const notNullValue = fName ?? lName;
