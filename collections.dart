c378b68508655792e2e43ff069326af321bbcc82
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-04 23:31:09 +0530

    Delete collections.dart

--- collections.dart
+++
@@ -1,7 +0,0 @@
-//List : contains homogeneous entities
-var names1 = ['foo','bar''abc']  
-  
-// Sets : contains unique entities
-var names2 = {'foo','bar''abc'}  
-names2.add('foo');
-print(names);      //code executes but 'foo' wont be added to the set
