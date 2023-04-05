afc0e344e3155f93a62501a856799148bebd4d46
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-05 23:07:34 +0530

    Update 3_if_else.dart

--- 3_if_else.dart
+++ 3_if_else.dart
@@ -1,4 +1,6 @@
 void main() {
+  
+  //if else statements
   var marks = 80
     
     if (marks > 60){
@@ -6,5 +8,30 @@ void main() {
     } else {
       print("You didn't pass ");
     }
+  
+  //swicth case
+  String s1 = '+';
+
+	switch (s1) {
+
+		case '+':
+			print("Addition");
+			break;
+
+		case '-':
+			print("Substraction");
+			break;
+
+		case '*':
+			print("Multiplication");
+			break;
+
+		case '/':
+			print("Division");
+			break;
+      
+		default:
+			print("Invalid Input");
+	}
   
 }
