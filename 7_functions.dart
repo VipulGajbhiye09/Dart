0452500f3f81b14d829d88a3eef181cfb222b28a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-06 23:41:41 +0530

    Update 7_functions.dart

--- 7_functions.dart
+++ 7_functions.dart
@@ -1,8 +1,20 @@
+//function 
 int getArea(int length, int breadth) {
 
 	int area = length * breadth;
 	return area;
 }
+
+//function shorthand
+int getArea(int length, int breadth) => length * breadth;    
+
+// "=> expression" is a short hand syntax for { return expression; }
+
+
+
+
+
+
 
 void main() {
 
