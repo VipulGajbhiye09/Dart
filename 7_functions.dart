4d65f886103c16526eb1f630e9027a757efd50b4
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-06 23:44:24 +0530

    Update 7_functions.dart

--- 7_functions.dart
+++ 7_functions.dart
@@ -10,9 +10,13 @@ int getArea(int length, int breadth) => length * breadth;
 
 // "=> expression" is a short hand syntax for { return expression; }
 
+// Required Parameters
+void printNames(String name1, String name2, String name3) {
 
-
-
+	print("Name 1 is $name1");
+	print("Name 2 is $name2");
+	print("Name 3 is $name3");
+}
 
 
 
@@ -21,4 +25,5 @@ void main() {
 	int rectArea = getArea(15, 4);
 	print("The area is $rectArea");
   
+	printNames("foo","bar","baz")
 }
