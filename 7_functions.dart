2e683aae23026d5759b2166ddd98f1fd32846790
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-06 23:46:34 +0530

    Update 7_functions.dart

--- 7_functions.dart
+++ 7_functions.dart
@@ -17,13 +17,20 @@ void printNames(String name1, String name2, String name3) {
 	print("Name 2 is $name2");
 	print("Name 3 is $name3");
 }
+// Optional Positional Parameters
+void printSurNames(String name1, [String name2, String name3]) {
 
-
+	print("Name 1 is $name1");
+	print("Name 2 is $name2");
+	print("Name 3 is $name3");
+}
 
 void main() {
 
 	int rectArea = getArea(15, 4);
 	print("The area is $rectArea");
   
-	printNames("foo","bar","baz")
+	printNames("foo","bar","baz");
+	
+	printSurnames("Sample");
 }
