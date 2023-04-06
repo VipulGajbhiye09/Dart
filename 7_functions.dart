bd798be03e278c20dcdf87f0f734d8bbdf7bb9b6
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-06 23:40:04 +0530

    Create 7_functions.dart

---
+++ 7_functions.dart
@@ -0,0 +1,12 @@
+int getArea(int length, int breadth) {
+
+	int area = length * breadth;
+	return area;
+}
+
+void main() {
+
+	int rectArea = getArea(15, 4);
+	print("The area is $rectArea");
+  
+}
