09b39fe11c63a333520ecc1dc8ff5e4e55687344
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-08 23:26:43 +0530

    Create 9_oop.dart

---
+++ 9_oop.dart
@@ -0,0 +1,12 @@
+// Classes : grouping of various functionalities into one entity
+
+class Person {
+  void breathe() {
+    //logic of breathe()  
+  }
+  
+  void run() {
+    //logic of run()  
+  }
+  
+}
