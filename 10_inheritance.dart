de5372e8a077938c9ead51d93979ac75e2e107ad
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-09 23:34:07 +0530

    Update 10_inheritance.dart

--- 10_inheritance.dart
+++ 10_inheritance.dart
@@ -7,11 +7,20 @@ class Animal {
 	}
 }
 
-class Dog extends Animal {     
+class Dog extends Animal {     // Dog is Child class or sub class, Animal is super or parent class
 
 	String dog_name;
 
 	void bark() {
 		print("Bark ");
+	}
+}
+
+class Cat extends Animal {      // Cat is Child class or sub class, Animal is super or parent class
+
+	int age;
+
+	void meow() {
+		print("Meow !");
 	}
 }
