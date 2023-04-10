3928b4c82d0280e22435529319d20ee9307996a7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-10 23:53:39 +0530

    Update 11_exception_handling.dart

--- 11_exception_handling.dart
+++ 11_exception_handling.dart
@@ -7,6 +7,13 @@ void main() {
 	} on IntegerDivisionByZeroException {
 		print("Cannot divide by Zero");
 	}
-  
+	
+ 	 // CASE 2: When the exception is unknown, we use CATCH Clause
+	try {
+		int result = 15 ~/ 0;
+		print("The result is $result");
+	} catch (e) {
+		print("The exception thrown is $e");
+	}
 }
   
