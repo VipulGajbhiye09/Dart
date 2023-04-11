ed27d5764322e45b27b8565d56bb1a3e2328352c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-11 23:58:45 +0530

    Update 11_exception_handling.dart

--- 11_exception_handling.dart
+++ 11_exception_handling.dart
@@ -15,5 +15,14 @@ void main() {
 	} catch (e) {
 		print("The exception thrown is $e");
 	}
+	
+	// CASE 3: Using STACK TRACE to know the events occurred before Exception was thrown
+	try {
+		int result = 15 ~/ 0;
+		print("The result is $result");
+	} catch (e, s) {
+		print("The exception thrown is $e");
+		print("STACK TRACE \n $s");
+	}
 }
   
