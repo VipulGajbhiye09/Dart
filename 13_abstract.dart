374155a2720e37c5af2d4e66c0b1e6522f332034
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-05 23:04:47 +0530

    Create 13_abstract.dart

---
+++ 13_abstract.dart
@@ -0,0 +1,20 @@
+void main() {
+
+	var a1 = Square();
+	a1.draw();
+
+	var a2 = Circle();
+	a2.draw();
+}
+
+abstract class Shape {   //Abstract class
+
+	int x;
+	int y;
+
+	void draw();        // Abstract Method
+
+	void sampleFunction() {
+		// Function logic 
+	}
+}
